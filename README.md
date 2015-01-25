bfb5d74d78b4f3035cbcdd6f2294daa2b599a416
Peter Wagener <peter@wagener.org>
2015-01-25 12:37:37 -0500

    Provide 'fetchWithFill' module & tests

--- README.md
+++ README.md
@@ -14,18 +14,20 @@ var MyCollection = Conduit.Collection.extend({
     // Your magic here
 });
 
-// Some enormous amount of data
-var aBigArray = [ ... ];
-
 var collection = new MyCollection();
+
+// If you have the data on the page ...
+var aBigArray = [ ... ];
 collection.refill(aBigArray);
-// ... and so on
+
+// Or, if you need to fetch it asynchronously
+collection.fetchJumbo();
 ```
 
 # More, Please
 Please check out [The Documentation](http://pwagener.github.io/backbone.conduit/).
 
-Is documentation boring to you?  Well, how about a
+Does documentation bore you?  Well, how about a
 [live demo of refill(...)](http://pwagener.github.io/backbone.conduit/examples/refill-vs-reset.html) showing the
 performance improvement versus `Backbone.Collection.reset(...)`.  Or perhaps a
 [live demo of fill(...)](http://pwagener.github.io/backbone.conduit/examples/fill-vs-set.html) allowing you to
--- dist/backbone.conduit.js
+++ dist/backbone.conduit.js
@@ -139,11 +139,11 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var refill = __webpack_require__(2);
 	var fill = __webpack_require__(6);
+	var fetchJumbo = __webpack_require__(7);
 
 	// Extend Backbone.Collection and provide the 'refill' method
 	var Collection = Backbone.Collection.extend({ });
-	refill.mixin(Collection);
-	fill.mixin(Collection);
+	fetchJumbo.mixin(Collection);
 
 	module.exports = Collection;
 
@@ -344,6 +344,71 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return Collection;
 	    }
 	};
+
+/***/ },
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _ = __webpack_require__(4);
+	var Backbone = __webpack_require__(1);
+	var fill = __webpack_require__(6);
+	var refill = __webpack_require__(2);
+
+	/**
+	 * This utility method is taken from backbone.js verbatim
+	 */
+	var wrapError = function(model, options) {
+	    var error = options.error;
+	    options.error = function(resp) {
+	        if (error) error(model, resp, options);
+	        model.trigger('error', model, resp, options);
+	    };
+	};
+
+	/**
+	 * This method is a replacement for Backbone.Collection.fetch that will use
+	 * Conduit.Collection.fill/refill instead of Backbone.Collection.set/reset when data
+	 * is successfully returned from the server.
+	 */
+	function fetchJumbo(options) {
+	    options = options ? _.clone(options) : {};
+	    if (options.parse === void 0) options.parse = true;
+	    var success = options.success;
+	    var collection = this;
+	    options.success = function(resp) {
+	        // This is the interesting line:  use refill/fill instead of reset/set
+	        var method = options.reset ? 'refill' : 'fill';
+	        collection[method](resp, options);
+	        if (success) success(collection, resp, options);
+	        collection.trigger('sync', collection, resp, options);
+	    };
+	    wrapError(this, options);
+	    return this.sync('read', this, options);
+	}
+
+	var mixinObj = {
+	    fetchJumbo: fetchJumbo
+	};
+
+
+	module.exports = {
+	    mixin: function(Collection) {
+
+	        if (!_.isFunction(Collection.prototype.refill)) {
+	            refill.mixin(Collection);
+	        }
+
+	        if (!_.isFunction(Collection.prototype.fill)) {
+	            fill.mixin(Collection);
+	        }
+
+	        _.extend(Collection.prototype, mixinObj);
+	        return Collection;
+	    }
+	};
+
 
 /***/ }
 /******/ ])
