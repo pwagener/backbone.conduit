6af97d0b167d108e839b95ee6c0d3f82354f2e34
Peter Wagener <peter@wagener.org>
2015-09-25 11:00:07 -0400

    Distributable files for v0.6.12

--- dist/backbone.conduit.js
+++ dist/backbone.conduit.js
@@ -771,11 +771,18 @@ return /******/ (function(modules) { // webpackBootstrap
 	    // ToDeprecate at the release of 0.7.X
 	    if (!sortSpec && this.comparator) {
 	        console.log('Warning: defining the sort specification as "collection.comparator" will be removed in the next release.  Use "collection.sortSpec" instead.');
+	        this.sortSpec = this.comparator;
 	    }
-	    sortSpec = sortSpec || this.sortSpec || this.comparator;
+	    sortSpec = sortSpec || this.sortSpec;
+
+	    // ToDeprecate at the release of 0.7.X
+	    if (sortSpec && sortSpec.evaluator) {
+	        console.log('Warning: defining the sort method with "evaluator" will be removed in the next release.  Use { method: "someMethod" } instead.');
+	        sortSpec.method = sortSpec.evaluator;
+	    }
 
 	    // Error if comparator isn't provided correctly.
-	    if (!sortSpec || (!sortSpec.property && !sortSpec.evaluator)) {
+	    if (!sortSpec || (!sortSpec.property && !sortSpec.method)) {
 	        return when.reject(new Error('Please provide a sort specification'));
 	    }
 
@@ -825,6 +832,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return when.reject(new Error('Please provide a filter specification'));
 	    }
 
+	    // ToDeprecate at the release of 0.7.X
+	    if (filterSpec.evaluator) {
+	        console.log('Warning: defining the filter method with "evaluator" will be removed in the next release.  Use { method: "someMethod" } instead.');
+	        filterSpec.method = filterSpec.evaluator;
+	    }
+
 	    return this._boss.makePromise({
 	        method: 'filter',
 	        arguments: [ filterSpec ]
@@ -858,6 +871,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	    var self = this;
 	    mapSpec = mapSpec || this.mapSpec;
 
+	    // ToDeprecate in 0.7.X
+	    if (_.isString(mapSpec)) {
+	        console.log('Warning: providing the "mapSpec" method name as a string will be removed in the next version; use { method: "someMethod" } instead');
+	        mapSpec = { method: mapSpec };
+	    }
+	    if (_.isString(mapSpec.mapper)) {
+	        console.log('Warning: providing the mapping method as "mapper" will be removed in the next version; use { method: "someMethod" } instead');
+	        mapSpec.method = mapSpec.mapper;
+	    }
+
 	    return this._boss.makePromise({
 	        method: 'map',
 	        arguments: [ mapSpec ]
@@ -878,6 +901,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	 */
 	function reduceAsync(reduceSpec) {
 	    _ensureBoss.call(this);
+
+	    // ToDeprecate in 0.7.X
+	    if (_.isString(reduceSpec.reducer)) {
+	        console.log('Warning: providing the reduce method as "reducer" will be removed in the next version; use { method: "someMethod" } instead');
+	        reduceSpec.method = reduceSpec.reducer;
+	    }
 
 	    return this._boss.makePromise({
 	        method: 'reduce',
--- examples/public/lib/conduit.worker.data.js
+++ examples/public/lib/conduit.worker.data.js
@@ -81,7 +81,7 @@
 	/**
 	 * This worker method handler stores data on the worker.
 	 */
-	var _ = __webpack_require__(14);
+	var _ = __webpack_require__(15);
 
 	var dataUtils = __webpack_require__(12);
 
@@ -114,7 +114,7 @@
 	 * Module used to merge existing data sets on the worker
 	 */
 
-	var _ = __webpack_require__(14);
+	var _ = __webpack_require__(15);
 
 	var dataUtils = __webpack_require__(12);
 
@@ -149,7 +149,7 @@
 	 * This worker method handler returns data from the worker.
 	 */
 
-	var _ = __webpack_require__(14);
+	var _ = __webpack_require__(15);
 
 	var dataUtils = __webpack_require__(12);
 
@@ -195,7 +195,7 @@
 	/**
 	 * This module provides sorting for the worker
 	 */
-	var _ = __webpack_require__(14);
+	var _ = __webpack_require__(15);
 	var dataUtils = __webpack_require__(12);
 
 	module.exports = {
@@ -211,14 +211,8 @@
 	            evaluator = function (item) {
 	                return item[property];
 	            }
-	        } else if (_.isString(sortSpec.method) || _.isString(sortSpec.evaluator)) {
-	            // ToDeprecate in 0.7.X
-	            if (_.isString(sortSpec.method)) {
-	                console.log('Warning:  sortAsync sorting method specified as "method" will be removed in the next release.  Use "evaluator" instead.');
-	                sortSpec.evaluator = sortSpec.method;
-	            }
-
-	            evaluator = ConduitWorker.handlers[sortSpec.evaluator];
+	        } else if (_.isString(sortSpec.method)) {
+	            evaluator = ConduitWorker.handlers[sortSpec.method];
 	        } else {
 	            throw new Error('Provide a property name as "comparator" or a registered method as { method }');
 	        }
@@ -248,7 +242,7 @@
 	/**
 	 * This module provides filtering for the worker.
 	 */
-	var _ = __webpack_require__(14);
+	var _ = __webpack_require__(15);
 	var dataUtils = __webpack_require__(12);
 
 	module.exports = {
@@ -262,9 +256,9 @@
 	    method: function(filterSpec) {
 
 	        var filterFunc;
-	        if (_.isString(filterSpec.evaluator)) {
+	        if (_.isString(filterSpec.method)) {
 	            // Find the evaluator from the registered components
-	            var evaluator = ConduitWorker.handlers[filterSpec.evaluator];
+	            var evaluator = ConduitWorker.handlers[filterSpec.method];
 
 	            if (_.isUndefined(evaluator)) {
 	                throw new Error('No registered handler found for "' + filterSpec + '"');
@@ -298,7 +292,7 @@
 	'use strict';
 
 
-	var _ = __webpack_require__(14);
+	var _ = __webpack_require__(15);
 	var dataUtils = __webpack_require__(12);
 
 	module.exports = {
@@ -306,13 +300,7 @@
 	    bindToWorker: true,
 
 	    method: function(mapSpec) {
-	        // ToDeprecate in 0.7.X
-	        if (_.isString(mapSpec)) {
-	            console.log('Warning: providing the "mapAsync" method name as a string will be removed in the next version; use { mapper: "someMethod" }');
-	            mapSpec = { mapper: mapSpec };
-	        }
-
-	        var mapFuncName = mapSpec.mapper;
+	        var mapFuncName = mapSpec.method;
 	        if (_.isString(mapFuncName)) {
 	            var mapper = ConduitWorker.handlers[mapFuncName];
 
@@ -344,7 +332,7 @@
 	/**
 	 * This worker module provides a 'reduce(...)' method.
 	 */
-	var _ = __webpack_require__(14);
+	var _ = __webpack_require__(15);
 	var dataUtils = __webpack_require__(12);
 
 	module.exports = {
@@ -396,7 +384,7 @@
 
 	'use strict';
 
-	var _ = __webpack_require__(14);
+	var _ = __webpack_require__(15);
 	var when = __webpack_require__(16);
 	var dataUtils = __webpack_require__(12);
 	var nanoAjax = __webpack_require__(13);
@@ -463,7 +451,7 @@
 	 * Implement POST and PUT REST-ful calls
 	 */
 
-	var _ = __webpack_require__(14);
+	var _ = __webpack_require__(15);
 	var when = __webpack_require__(16);
 	var dataUtils = __webpack_require__(12);
 	var nanoAjax = __webpack_require__(13);
@@ -527,7 +515,7 @@
 	 * Implement DELETE REST-ful calls
 	 */
 
-	var _ = __webpack_require__(14);
+	var _ = __webpack_require__(15);
 	var when = __webpack_require__(16);
 	var dataUtils = __webpack_require__(12);
 	var nanoAjax = __webpack_require__(13);
@@ -616,8 +604,8 @@
 	 * that allow us to not constantly iterate over the whole set.
 	 */
 
-	var _ = __webpack_require__(14);
-	var managedContext = __webpack_require__(15);
+	var _ = __webpack_require__(15);
+	var managedContext = __webpack_require__(14);
 
 	function _getContext(skipInit) {
 	    if (!ConduitWorker._data && !skipInit) {
@@ -942,6 +930,228 @@
 
 /***/ },
 /* 14 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(global) {'use strict';
+
+	/**
+	 * Collection of methods for working with/managing the worker context.  Externally, this module creates
+	 * the 'ConduitWorker' namespace that worker methods use to plug themselves into Conduit.  Internally,
+	 * this module sets up the 'onmessage(..)' and 'postMessage(...)' methods in the global Worker context to allow it
+	 * to communicate with the main thread.
+	 */
+
+	var _ = __webpack_require__(15);
+	var util = __webpack_require__(31);
+	var when = __webpack_require__(16);
+
+	var managedContext;
+
+	/**
+	 * Function to register a new plugin component with Conduit.  This is bound to the ConduitWorker context.
+	 * @param component The component to register.  Should contain:
+	 *   - name:  The component name
+	 *   - handlers: Array of the handlers to enable
+	 *   -
+	 * @private
+	 */
+	function _registerComponent(component) {
+	    var name = component.name;
+	    if (!name) {
+	        throw new Error('Conduit component must have a "name"');
+	    } else {
+	        var methods = component.methods || [];
+	        debug('Registering component "' + name + '" (' + methods.length + ' methods)');
+	        this.components[name] = component;
+	        _enableHandlers(this, methods);
+	    }
+	}
+
+	/**
+	 * Function to register additional method handlers to the worker.  This is bound
+	 * to the ConduitWorker context.
+	 * @param context the context to add the handlers to
+	 * @param handlerModules An array of handler modules to consider when receiving a message
+	 */
+	function _enableHandlers(context, handlerModules) {
+
+	    // Set up handlers for all the methods we currently support
+	    var handlers = context.handlers;
+	    _.each(handlerModules, function(handler) {
+	        var name = handler.name;
+
+	        if (!_.isString(name)) {
+	            throw new Error('Handler did not provide a name');
+	        }
+
+	        var method;
+	        if (handler.bindToWorker) {
+	            method = _.bind(handler.method, context);
+	        } else {
+	            method = handler.method;
+	        }
+
+	        if (_.isUndefined(method)) {
+	            throw new Error('Handler "' + name + '" did not provide a "method"');
+	        }
+
+	        handlers[handler.name] = method;
+	    });
+	}
+
+	/**
+	 * The function we use to handle messages passed into the worker
+	 * @param event The message event
+	 * @private
+	 */
+	function _onMessage(event) {
+	    var method = event.data.method;
+	    var args = event.data.arguments;
+
+	    var ConduitWorker = _getConduitWorker();
+	    var handler = ConduitWorker.handlers[method];
+	    if (handler) {
+	        debug('Executing "' + method + '"');
+	        var result = handler.apply(ConduitWorker, args);
+
+	        // If a promise is returned from a handler we want to wait for it to resolve, so ...
+	        if (when.isPromiseLike(result)) {
+	            result.then(function(promiseResult) {
+	                _onCallComplete(event.data, promiseResult);
+	            });
+	        } else {
+	            _onCallComplete(event.data, result);
+	        }
+
+	    } else {
+	        var msg = "No such Conduit worker method: '" + method + "'";
+	        debug(msg);
+	        managedContext.postMessage(new Error(msg));
+	    }
+	}
+
+	function _onCallComplete(eventData, result) {
+	    managedContext.postMessage(result);
+	    debug('Completed "' + eventData.method + '"');
+	}
+
+	function _initContext(optionalContext) {
+	    var context = managedContext = optionalContext || this || global;
+
+	    if (!context) {
+	        throw new Error('Failed to find worker managed context');
+	    }
+
+	    if (!context.ConduitWorker) {
+	        var ConduitWorker = context.ConduitWorker = {
+	            // The configuration of this worker
+	            config: {},
+
+	            // The set of registered components
+	            components: {},
+
+	            // The handlers we may use to process a message from the main thread.
+	            handlers: {},
+
+	            // Method that allows components to print a debug message:
+	            debug: debug
+	        };
+
+	        // Method that components can use to add their own method handlers
+	        ConduitWorker.registerComponent = _.bind(_registerComponent, ConduitWorker);
+	        context.onmessage = _onMessage;
+
+	        debug('Initialized ConduitWorker');
+	    }
+	}
+
+
+
+	function debug(msg) {
+	    if (_getConduitWorker().config.debug) {
+	        var currentdate = new Date();
+	        var now = currentdate.getHours() + ":"
+	            + currentdate.getMinutes() + ":"
+	            + currentdate.getSeconds() + '-' + currentdate.getMilliseconds();
+	        var line = now + ' conduit.worker: ' + msg;
+	        managedContext.console.log(line);
+	    }
+	}
+
+	/**
+	 * Retrieve the ConduitWorker namespace, which (unless it has been "set(...)"), will be the global
+	 * context.
+	 * @return {*}
+	 * @private
+	 */
+	function _getConduitWorker() {
+	    if (!managedContext) {
+	        _initContext();
+	    }
+
+	    return managedContext.ConduitWorker;
+	}
+
+	function setAsGlobal(context) {
+	    _initContext(context);
+	}
+
+
+	function configure(config) {
+	    config = config || {};
+
+	    var conduitWorker = _getConduitWorker();
+	    conduitWorker.config = config;
+
+	    // Import any component that is listed in the configuration
+	    _.each(config.components, function(component) {
+	        debug('Loading component: ' + component);
+	        managedContext.importScripts(component);
+	    });
+
+	    debug('ConduitWorker context configured: ' + util.inspect(config));
+
+	}
+
+	function enableCoreHandlers() {
+	    var conduitWorker = _getConduitWorker();
+	    conduitWorker.registerComponent({
+	        name: 'core',
+	        methods: [
+	            __webpack_require__(17),
+	            __webpack_require__(18)
+	        ]
+	    });
+	}
+
+	module.exports = {
+
+	    /**
+	     * Set the global context; this is only useful for testing.
+	     */
+	    setAsGlobal: setAsGlobal,
+
+	    /**
+	     * Method to enable the built-in method handlers we expose
+	     */
+	    enableCoreHandlers: enableCoreHandlers,
+
+	    /**
+	     * Set the configuration for the context
+	     *
+	     */
+	    configure: configure,
+
+	    /**
+	     * Write a debug message (if we have been configured to do so)
+	     */
+	    debug: debug
+
+	};
+	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
+
+/***/ },
+/* 15 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;//     Underscore.js 1.8.3
@@ -2495,228 +2705,6 @@
 
 
 /***/ },
-/* 15 */
-/***/ function(module, exports, __webpack_require__) {
-
-	/* WEBPACK VAR INJECTION */(function(global) {'use strict';
-
-	/**
-	 * Collection of methods for working with/managing the worker context.  Externally, this module creates
-	 * the 'ConduitWorker' namespace that worker methods use to plug themselves into Conduit.  Internally,
-	 * this module sets up the 'onmessage(..)' and 'postMessage(...)' methods in the global Worker context to allow it
-	 * to communicate with the main thread.
-	 */
-
-	var _ = __webpack_require__(14);
-	var util = __webpack_require__(31);
-	var when = __webpack_require__(16);
-
-	var managedContext;
-
-	/**
-	 * Function to register a new plugin component with Conduit.  This is bound to the ConduitWorker context.
-	 * @param component The component to register.  Should contain:
-	 *   - name:  The component name
-	 *   - handlers: Array of the handlers to enable
-	 *   -
-	 * @private
-	 */
-	function _registerComponent(component) {
-	    var name = component.name;
-	    if (!name) {
-	        throw new Error('Conduit component must have a "name"');
-	    } else {
-	        var methods = component.methods || [];
-	        debug('Registering component "' + name + '" (' + methods.length + ' methods)');
-	        this.components[name] = component;
-	        _enableHandlers(this, methods);
-	    }
-	}
-
-	/**
-	 * Function to register additional method handlers to the worker.  This is bound
-	 * to the ConduitWorker context.
-	 * @param context the context to add the handlers to
-	 * @param handlerModules An array of handler modules to consider when receiving a message
-	 */
-	function _enableHandlers(context, handlerModules) {
-
-	    // Set up handlers for all the methods we currently support
-	    var handlers = context.handlers;
-	    _.each(handlerModules, function(handler) {
-	        var name = handler.name;
-
-	        if (!_.isString(name)) {
-	            throw new Error('Handler did not provide a name');
-	        }
-
-	        var method;
-	        if (handler.bindToWorker) {
-	            method = _.bind(handler.method, context);
-	        } else {
-	            method = handler.method;
-	        }
-
-	        if (_.isUndefined(method)) {
-	            throw new Error('Handler "' + name + '" did not provide a "method"');
-	        }
-
-	        handlers[handler.name] = method;
-	    });
-	}
-
-	/**
-	 * The function we use to handle messages passed into the worker
-	 * @param event The message event
-	 * @private
-	 */
-	function _onMessage(event) {
-	    var method = event.data.method;
-	    var args = event.data.arguments;
-
-	    var ConduitWorker = _getConduitWorker();
-	    var handler = ConduitWorker.handlers[method];
-	    if (handler) {
-	        debug('Executing "' + method + '"');
-	        var result = handler.apply(ConduitWorker, args);
-
-	        // If a promise is returned from a handler we want to wait for it to resolve, so ...
-	        if (when.isPromiseLike(result)) {
-	            result.then(function(promiseResult) {
-	                _onCallComplete(event.data, promiseResult);
-	            });
-	        } else {
-	            _onCallComplete(event.data, result);
-	        }
-
-	    } else {
-	        var msg = "No such Conduit worker method: '" + method + "'";
-	        debug(msg);
-	        managedContext.postMessage(new Error(msg));
-	    }
-	}
-
-	function _onCallComplete(eventData, result) {
-	    managedContext.postMessage(result);
-	    debug('Completed "' + eventData.method + '"');
-	}
-
-	function _initContext(optionalContext) {
-	    var context = managedContext = optionalContext || this || global;
-
-	    if (!context) {
-	        throw new Error('Failed to find worker managed context');
-	    }
-
-	    if (!context.ConduitWorker) {
-	        var ConduitWorker = context.ConduitWorker = {
-	            // The configuration of this worker
-	            config: {},
-
-	            // The set of registered components
-	            components: {},
-
-	            // The handlers we may use to process a message from the main thread.
-	            handlers: {},
-
-	            // Method that allows components to print a debug message:
-	            debug: debug
-	        };
-
-	        // Method that components can use to add their own method handlers
-	        ConduitWorker.registerComponent = _.bind(_registerComponent, ConduitWorker);
-	        context.onmessage = _onMessage;
-
-	        debug('Initialized ConduitWorker');
-	    }
-	}
-
-
-
-	function debug(msg) {
-	    if (_getConduitWorker().config.debug) {
-	        var currentdate = new Date();
-	        var now = currentdate.getHours() + ":"
-	            + currentdate.getMinutes() + ":"
-	            + currentdate.getSeconds() + '-' + currentdate.getMilliseconds();
-	        var line = now + ' conduit.worker: ' + msg;
-	        managedContext.console.log(line);
-	    }
-	}
-
-	/**
-	 * Retrieve the ConduitWorker namespace, which (unless it has been "set(...)"), will be the global
-	 * context.
-	 * @return {*}
-	 * @private
-	 */
-	function _getConduitWorker() {
-	    if (!managedContext) {
-	        _initContext();
-	    }
-
-	    return managedContext.ConduitWorker;
-	}
-
-	function setAsGlobal(context) {
-	    _initContext(context);
-	}
-
-
-	function configure(config) {
-	    config = config || {};
-
-	    var conduitWorker = _getConduitWorker();
-	    conduitWorker.config = config;
-
-	    // Import any component that is listed in the configuration
-	    _.each(config.components, function(component) {
-	        debug('Loading component: ' + component);
-	        managedContext.importScripts(component);
-	    });
-
-	    debug('ConduitWorker context configured: ' + util.inspect(config));
-
-	}
-
-	function enableCoreHandlers() {
-	    var conduitWorker = _getConduitWorker();
-	    conduitWorker.registerComponent({
-	        name: 'core',
-	        methods: [
-	            __webpack_require__(17),
-	            __webpack_require__(18)
-	        ]
-	    });
-	}
-
-	module.exports = {
-
-	    /**
-	     * Set the global context; this is only useful for testing.
-	     */
-	    setAsGlobal: setAsGlobal,
-
-	    /**
-	     * Method to enable the built-in method handlers we expose
-	     */
-	    enableCoreHandlers: enableCoreHandlers,
-
-	    /**
-	     * Set the configuration for the context
-	     *
-	     */
-	    configure: configure,
-
-	    /**
-	     * Write a debug message (if we have been configured to do so)
-	     */
-	    debug: debug
-
-	};
-	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
-
-/***/ },
 /* 16 */
 /***/ function(module, exports, __webpack_require__) {
 
@@ -2978,7 +2966,7 @@
 	/**
 	 * This module allows you to pass a configuration into the worker's context
 	 */
-	var managedContext = __webpack_require__(15);
+	var managedContext = __webpack_require__(14);
 	var util = __webpack_require__(31);
 
 	module.exports = {
