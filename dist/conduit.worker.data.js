bf199ad2eca4606fd7a2e78b6e55e749f78fa8a9
pwagener <peter@wagener.org>
2016-05-17 20:14:40 -0400

    Version bump

--- dist/conduit.worker.data.js
+++ dist/conduit.worker.data.js
@@ -386,7 +386,7 @@
 
 	var _ = __webpack_require__(15);
 	var dataUtils = __webpack_require__(12);
-	var nanoAjax = __webpack_require__(13);
+	var nanoAjax = __webpack_require__(14);
 
 	module.exports = {
 	    name: 'restGet',
@@ -452,7 +452,7 @@
 
 	var _ = __webpack_require__(15);
 	var dataUtils = __webpack_require__(12);
-	var nanoAjax = __webpack_require__(13);
+	var nanoAjax = __webpack_require__(14);
 
 	module.exports = {
 	    name: 'restSave',
@@ -515,7 +515,7 @@
 
 	var _ = __webpack_require__(15);
 	var dataUtils = __webpack_require__(12);
-	var nanoAjax = __webpack_require__(13);
+	var nanoAjax = __webpack_require__(14);
 
 	module.exports = {
 	    name: 'restDestroy',
@@ -602,7 +602,7 @@
 	 */
 
 	var _ = __webpack_require__(15);
-	var managedContext = __webpack_require__(14);
+	var managedContext = __webpack_require__(13);
 
 	function _getContext(skipInit) {
 	    if (!ConduitWorker._data && !skipInit) {
@@ -879,56 +879,6 @@
 /* 13 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/* WEBPACK VAR INJECTION */(function(global) {exports.ajax = function (params, callback) {
-	  if (typeof params == 'string') params = {url: params}
-	  var headers = params.headers || {}
-	    , body = params.body
-	    , method = params.method || (body ? 'POST' : 'GET')
-	    , withCredentials = params.withCredentials || false
-
-	  var req = getRequest()
-
-	  req.onreadystatechange = function () {
-	    if (req.readyState == 4)
-	      callback(req.status, req.responseText, req)
-	  }
-
-	  if (body) {
-	    setDefault(headers, 'X-Requested-With', 'XMLHttpRequest')
-	    setDefault(headers, 'Content-Type', 'application/x-www-form-urlencoded')
-	  }
-
-	  req.open(method, params.url, true)
-
-	  // has no effect in IE
-	  // has no effect for same-origin requests
-	  // has no effect in CORS if user has disabled 3rd party cookies
-	  req.withCredentials = withCredentials
-
-	  for (var field in headers)
-	    req.setRequestHeader(field, headers[field])
-
-	  req.send(body)
-	}
-
-	function getRequest() {
-	  if (global.XMLHttpRequest)
-	    return new global.XMLHttpRequest;
-	  else
-	    try { return new global.ActiveXObject("MSXML2.XMLHTTP.3.0"); } catch(e) {}
-	  throw new Error('no xmlhttp request able to be created')
-	}
-
-	function setDefault(obj, key, value) {
-	  obj[key] = obj[key] || value
-	}
-	
-	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
-
-/***/ },
-/* 14 */
-/***/ function(module, exports, __webpack_require__) {
-
 	/* WEBPACK VAR INJECTION */(function(global) {'use strict';
 
 	/**
@@ -1166,6 +1116,56 @@
 	    debug: debug
 
 	};
+	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
+
+/***/ },
+/* 14 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(global) {exports.ajax = function (params, callback) {
+	  if (typeof params == 'string') params = {url: params}
+	  var headers = params.headers || {}
+	    , body = params.body
+	    , method = params.method || (body ? 'POST' : 'GET')
+	    , withCredentials = params.withCredentials || false
+
+	  var req = getRequest()
+
+	  req.onreadystatechange = function () {
+	    if (req.readyState == 4)
+	      callback(req.status, req.responseText, req)
+	  }
+
+	  if (body) {
+	    setDefault(headers, 'X-Requested-With', 'XMLHttpRequest')
+	    setDefault(headers, 'Content-Type', 'application/x-www-form-urlencoded')
+	  }
+
+	  req.open(method, params.url, true)
+
+	  // has no effect in IE
+	  // has no effect for same-origin requests
+	  // has no effect in CORS if user has disabled 3rd party cookies
+	  req.withCredentials = withCredentials
+
+	  for (var field in headers)
+	    req.setRequestHeader(field, headers[field])
+
+	  req.send(body)
+	}
+
+	function getRequest() {
+	  if (global.XMLHttpRequest)
+	    return new global.XMLHttpRequest;
+	  else
+	    try { return new global.ActiveXObject("MSXML2.XMLHTTP.3.0"); } catch(e) {}
+	  throw new Error('no xmlhttp request able to be created')
+	}
+
+	function setDefault(obj, key, value) {
+	  obj[key] = obj[key] || value
+	}
+	
 	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
 
 /***/ },
@@ -2750,7 +2750,7 @@
 	/**
 	 * This module allows you to pass a configuration into the worker's context
 	 */
-	var managedContext = __webpack_require__(14);
+	var managedContext = __webpack_require__(13);
 	var util = __webpack_require__(18);
 
 	module.exports = {
